0508317858bbc5aa6a69c848ff15cb2ac2af3f43
lucerogr <55417973+lucerogr@users.noreply.github.com>
2019-09-27 17:58:20 -0500

    Update Controlador.h

--- Controlador.h
+++ Controlador.h
@@ -3,45 +3,57 @@
 #include "MainForm.h"
 using namespace System;
 using namespace System::Drawing;
+
 //#include "Personaje.h"
 class Controlador {
 protected:
+	bool turno;
+	Heroe*objHeroe;
 public:
-	Controlador() {}
+	Controlador() { turno = true;	objHeroe = new Heroe();	}
 	~Controlador() {}
-	void Comprobar(Graphics^g, Heroe *objHeroe, Enemigo *malo1, Bitmap^heroeimg, Bitmap^maloimg, bool turno, bool defendido, bool item_defendido, System::Windows::Forms::ProgressBar^ Barra_Heroe, System::Windows::Forms::TextBox^  textBox2, System::Windows::Forms::PictureBox^  pictureBox1){
+	
+	void Start(int it, bool dificultad, Enemigo** arrMalo, Graphics^g, Heroe *objHeroe, Bitmap^heroeimg, Bitmap^maloimg, bool defendido, bool item_defendido, System::Windows::Forms::ProgressBar^ Barra_Heroe, System::Windows::Forms::TextBox^  textBox2, System::Windows::Forms::PictureBox^  pictureBox1){
+		
+		
+		
+
 		if (objHeroe->getvida() > 0) {
 			objHeroe->mover(g, heroeimg);
 		}
-		if (malo1->getvida() > 0) {
-			malo1->mover(g, maloimg);
+		if (arrMalo[it]->getvida() > 0) {
+			arrMalo[it]->mover(g, maloimg);
 		}
 
-		if (malo1->getvida() <= 0) {
-			malo1->~Enemigo();
+		if (arrMalo[it]->getvida() <= 0) {
+			arrMalo[it]->~Enemigo();
 			pictureBox1->Visible = true;
 		}
 		else {
 			if (turno == false) {
 				if (defendido == true) {
 					if (item_defendido == true) {
-						objHeroe->setvida(objHeroe->getvida() - (malo1->getFuerza() / 4));
-						Barra_Heroe->Increment((malo1->getFuerza() / 4) * -1);      
-						textBox2->Text = ((malo1->getFuerza() / 4)* -1).ToString();
+						objHeroe->setvida(objHeroe->getvida() - (arrMalo[it]->getFuerza() / 4));
+						Barra_Heroe->Increment((arrMalo[it]->getFuerza() / 4) * -1);      
+						textBox2->Text = ((arrMalo[it]->getFuerza() / 4)* -1).ToString();
 					}
 					else {
-						objHeroe->setvida(objHeroe->getvida() - (malo1->getFuerza() / 2));
-						Barra_Heroe->Increment((malo1->getFuerza() / 2) * -1);
-						textBox2->Text = ((malo1->getFuerza() / 2) * -1).ToString();
+						objHeroe->setvida(objHeroe->getvida() - (arrMalo[it]->getFuerza() / 2));
+						Barra_Heroe->Increment((arrMalo[it]->getFuerza() / 2) * -1);
+						textBox2->Text = ((arrMalo[it]->getFuerza() / 2) * -1).ToString();
 					}
 				}
 				else {
-					objHeroe->setvida(objHeroe->getvida() - malo1->getFuerza());
-					Barra_Heroe->Increment(malo1->getFuerza() * -1);
-					textBox2->Text = (malo1->getFuerza() * -1).ToString();
+					objHeroe->setvida(objHeroe->getvida() - arrMalo[it]->getFuerza());
+					Barra_Heroe->Increment(arrMalo[it]->getFuerza() * -1);
+					textBox2->Text = (arrMalo[it]->getFuerza() * -1).ToString();
 				}
 				turno = true;
 			}
 		}
 	}
+
+	Heroe *getHeroe() { return objHeroe; }
+	bool getTurno() { return turno; }
+	void setTurno(bool _turno) { this->turno = _turno; }
 };
