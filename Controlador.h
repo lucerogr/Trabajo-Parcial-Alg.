3aeda28add7d69649a571f83780bd5fc74e3eb71
lucerogr <55417973+lucerogr@users.noreply.github.com>
2019-09-27 21:41:48 -0500

    Update Controlador.h

--- Controlador.h
+++ Controlador.h
@@ -9,11 +9,14 @@ class Controlador {
 protected:
 	bool turno;
 	Heroe*objHeroe;
+	int cont_item_Vida;
+	int cont_item_Defendido;
+	bool defendido, item_Defendido;
 public:
-	Controlador() { turno = true;	objHeroe = new Heroe();	}
+	Controlador() { turno = true; objHeroe = new Heroe();	cont_item_Vida = 0; cont_item_Defendido = 0; defendido = false; item_Defendido = false; }
 	~Controlador() {}
 	
-	void Start(int it, bool dificultad, Enemigo** arrMalo, Graphics^g, Heroe *objHeroe, Bitmap^heroeimg, Bitmap^maloimg, bool defendido, bool item_defendido, System::Windows::Forms::ProgressBar^ Barra_Heroe, System::Windows::Forms::TextBox^  textBox2, System::Windows::Forms::PictureBox^  pictureBox1){
+	void Start(int it, Enemigo** arrMalo, Graphics^g, Heroe *objHeroe, Bitmap^heroeimg, Bitmap^maloimg, bool defendido, bool item_defendido, System::Windows::Forms::ProgressBar^ Barra_Heroe, System::Windows::Forms::TextBox^  textBox2, System::Windows::Forms::PictureBox^  pictureBox1){
 
 		objHeroe->mover(g, heroeimg);
 		
@@ -27,22 +30,27 @@ public:
 		}
 		else {
 			if (turno == false) {
-				if (defendido == true) {
-					if (item_defendido == true) {
+				//-----------------------Comprueba si ha clickeado Boton defendido-------------------//
+				if (getDefendido()) {
+					if (getItem_Def() == true){
 						objHeroe->setvida(objHeroe->getvida() - (arrMalo[it]->getFuerza() / 4));
 						Barra_Heroe->Increment((arrMalo[it]->getFuerza() / 4) * -1);      
 						textBox2->Text = ((arrMalo[it]->getFuerza() / 4)* -1).ToString();
+						setItem_Def(false);
 					}
 					else {
 						objHeroe->setvida(objHeroe->getvida() - (arrMalo[it]->getFuerza() / 2));
 						Barra_Heroe->Increment((arrMalo[it]->getFuerza() / 2) * -1);
 						textBox2->Text = ((arrMalo[it]->getFuerza() / 2) * -1).ToString();
 					}
+					setDefendido(false);
 				}
+				//-----------------------------------------------------------------------------------//
 				else {
 					objHeroe->setvida(objHeroe->getvida() - arrMalo[it]->getFuerza());
 					Barra_Heroe->Increment(arrMalo[it]->getFuerza() * -1);
 					textBox2->Text = (arrMalo[it]->getFuerza() * -1).ToString();
+
 				}
 				turno = true;
 			}
@@ -52,4 +60,16 @@ public:
 	Heroe *getHeroe() { return objHeroe; }
 	bool getTurno() { return turno; }
 	void setTurno(bool _turno) { this->turno = _turno; }
+
+	int getCont_item_Vida() { return cont_item_Vida; }
+	void setCont_item_Vida(int _cont) { this->cont_item_Vida = _cont; }
+
+	int getCont_item_Defendido() { return cont_item_Defendido; }
+	void setCont_item_Defendido(int _cont) { this->cont_item_Defendido = _cont; }
+
+	bool getDefendido() { return defendido; }
+	void setDefendido(bool _defendido) { this->defendido = _defendido; }
+
+	bool getItem_Def() { return item_Defendido; }
+	void setItem_Def(bool _item_def) { this->item_Defendido = _item_def; }
 };
